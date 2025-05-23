f9c97770603a624e1c9bdbd61b0d606bb4255452
Herm van den Akker <herman@ijssalonvincenzo.nl>
2025-05-23 16:19:42 +0200

    Gebruik app.run() voor Dash 3+

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ app.layout = html.Div([
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
+
